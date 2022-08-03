70f3249dff9bb7e73ff3020842d45cecb790bca0
raisin-toast <tyler.reysenbach@gmail.com>
2022-08-03 16:11:25 +1000

    add temp holders download function

--- R/download_from_data_gov.R
+++ R/download_from_data_gov.R
@@ -1,11 +1,13 @@
-res_temp_visa <- ckanr::resource_search(
-  url = "https://data.gov.au",
-  q = "name:temporary visa"
-)
+download_temp_visa_holders_data_gov <- function(search_term = "name:temporary visa",
+                                                path = "data/DoHA_temp_visa_holders.xlsx") {
+  res_temp_visa <- ckanr::resource_search(
+    url = "https://data.gov.au",
+    q = search_term
+  )
 
-ckan_fetch(res_temp_visa$results[[1]]$url,
-           store = "disk",
-           path = "data/DoHA_temp_visa_holders.xlsx"
-)
+  ckanr::ckan_fetch(res_temp_visa$results[[1]]$url,
+             store = "disk",
+             path = path
+  )
+}
 
-change
