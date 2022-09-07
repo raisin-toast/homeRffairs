9ae603dfdb8ecabe8e9204a75f97e3b1d4653db4
raisin-toast <73819156+raisin-toast@users.noreply.github.com>
2022-09-07 12:16:59 +1000

    more init and add

--- R/download_from_data_gov.R
+++ R/download_from_data_gov.R
@@ -3,10 +3,9 @@
 #' @param search_term (default = "name:temporary visa", character) the CKAN search term you are using to look for
 #' @param path (default = "data/DoHA_temp_visa_holders.xlsx"; character) the filepath where to save the data
 #'
-#' @return
+#' @return NULL
 #' @export
-#'
-#' @examples
+
 download_data_gov <- function(search_term = "name:temporary visa",
                                                 path = "data/DoHA_temp_visa_holders.xlsx") {
   res_temp_visa <- ckanr::resource_search(
@@ -20,3 +19,40 @@ download_data_gov <- function(search_term = "name:temporary visa",
   )
 }
 
+
+
+#' Download the temporary visa holders data from data.gov and clean it
+#'
+#' This function cleans the data.gov.au  data. However, home affairs locks its data behind a
+#' pivot table. If you need more detailed information, set the argument to TRUE. This will open the
+#' excel sheet. Pull down the additional variables you need and then save the file. The function will return the
+#' more detailed data
+#'
+#'
+#' @param path (character) where to save the file
+#' @param detailed (logical; default = FALSE)
+#'
+#' @return a dataframe
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{get_temp_visa_holders}
+get_temp_visa_holders <- function(path,
+                                  detailed = FALSE) {
+  download_data_gov(search_term = "name:temporary visa",
+                    path = path)
+
+  if (detailed){
+    fs::file_show(path)
+
+    usethis::ui_yeah("Have you expanded all the subcategories you need in the pivot table?")
+  }
+
+  readxl::read_excel(path,
+             sheet = "Visa Holders Pivot Table",
+             skip = 15) %>%
+  tidyr::pivot_longer(cols = tidyselect::contains("4"), names_to = "date", values_to = "num_holders") %>%
+  janitor::clean_names() %>%
+  dplyr::mutate(date = janitor::excel_numeric_to_date(readr::parse_number(date)))
+}
